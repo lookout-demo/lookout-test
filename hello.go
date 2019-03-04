a07d0058c3bcf5ca1f43a60810002239128d5b0b
Rafael Porres Molina <rafa@sourced.tech>
2019-03-04 22:53:18 +0100

    Update hello.go

--- hello.go
+++ hello.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world hello world hello world hello world hello world hello world hello world hello world hello worldhello world hello world hello world")
+	fmt.Println("even more and a longer hello world hello world hello world hello world hello world hello world hello world hello world hello worldhello world hello world hello world")
 }
