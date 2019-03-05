82e2799fdfe1aff0bd3d6c64e17528a1d4903757
Rafael Porres Molina <rafa@sourced.tech>
2019-03-05 20:27:24 +0100

    moar hello
    Signed-off-by: Rafael Porres Molina <rafa@sourced.tech>

--- hello.go
+++ hello.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("even more and a longer hello world hello world hello world hello world hello world hello world hello world hello world hello worldhello world hello world hello world")
+	fmt.Println("moar hello even more and a longer hello world hello world hello world hello world hello world hello world hello world hello world hello worldhello world hello world hello world")
 }
